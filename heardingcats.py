87ddb5a52167029c2864dbc4f59a3c29900e9dde
vincecr0ft <vincecroft@gmail.com>
2019-06-01 16:00:17 +0000

    adding keras transfer learning intro

---
+++ heardingcats.py
@@ -0,0 +1,51 @@
+import glob
+import numpy as np
+import os
+import shutil
+from utils import log_progress
+
+np.random.seed(42)
+
+def heard():
+    files = glob.glob('train/*')
+
+    cat_files = [fn for fn in files if 'cat' in fn]
+    dog_files = [fn for fn in files if 'dog' in fn]
+    len(cat_files), len(dog_files)
+
+    cat_train = np.random.choice(cat_files, size=1500, replace=False)
+    dog_train = np.random.choice(dog_files, size=1500, replace=False)
+    cat_files = list(set(cat_files) - set(cat_train))
+    dog_files = list(set(dog_files) - set(dog_train))
+
+    cat_val = np.random.choice(cat_files, size=500, replace=False)
+    dog_val = np.random.choice(dog_files, size=500, replace=False)
+    cat_files = list(set(cat_files) - set(cat_val))
+    dog_files = list(set(dog_files) - set(dog_val))
+
+    cat_test = np.random.choice(cat_files, size=500, replace=False)
+    dog_test = np.random.choice(dog_files, size=500, replace=False)
+
+    print('Cat datasets:', cat_train.shape, cat_val.shape, cat_test.shape)
+    print('Dog datasets:', dog_train.shape, dog_val.shape, dog_test.shape)
+
+    train_dir = 'training_data'
+    val_dir = 'validation_data'
+    test_dir = 'test_data'
+
+    train_files = np.concatenate([cat_train, dog_train])
+    validate_files = np.concatenate([cat_val, dog_val])
+    test_files = np.concatenate([cat_test, dog_test])
+
+    os.mkdir(train_dir) if not os.path.isdir(train_dir) else None
+    os.mkdir(val_dir) if not os.path.isdir(val_dir) else None
+    os.mkdir(test_dir) if not os.path.isdir(test_dir) else None
+
+    for fn in log_progress(train_files, name='Training Images'):
+        shutil.copy(fn, train_dir)
+
+    for fn in log_progress(validate_files, name='Validation Images'):
+        shutil.copy(fn, val_dir)
+
+    for fn in log_progress(test_files, name='Test Images'):
+        shutil.copy(fn, test_dir)
